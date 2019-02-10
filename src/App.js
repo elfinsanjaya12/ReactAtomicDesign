205e40aae8f1415fb21b1bc2c1a6fdb637799925
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-02-10 23:46:38 +0700

    Masih Belum Bener

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/organisms/Navbar";
-import Home from "./pages/Home";
-import PostsIndex from "./pages/posts/Index";
 
 class App extends Component {
   render() {
@@ -11,12 +9,6 @@ class App extends Component {
         <div>
           <div className="App">
             <Navbar />
-          </div>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/posts" exact component={PostsIndex} />
-            </Switch>
           </div>
         </div>
       </Router>
--- src/components/organisms/Navbar.js
+++ src/components/organisms/Navbar.js
@@ -1,55 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-class Navbar extends React.Component {
+class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="www.google.com">
-          Atomic Design
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+      <div className="container">
+        <nav class="navbar navbar-expand-sm navbar-custom">
+          <a class="navbar-brand" href="#">
+            GamaTextile
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/posts">
-                Posts
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">
-                Register
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  Home <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <div class="dropdown-divider" />
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     );
   }
 }
